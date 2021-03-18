b79693e4717d7834ad83a97bd84e4ff4ef2d32c7
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-18 23:05:33 +0000

    Initial Commit

---
+++ dCalc/dCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  dCalcApp.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct dCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
