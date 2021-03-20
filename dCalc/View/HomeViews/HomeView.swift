7f963dc5e7fc0d89177ea304609e7970163a898f
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-20 16:24:13 +0000

    add colorScheme to HomeView

--- dCalc/View/HomeViews/HomeView.swift
+++ dCalc/View/HomeViews/HomeView.swift
@@ -7,45 +7,76 @@
 
 import SwiftUI
 
-//TODO: Put this button in the middle and make it expandable
-//ADD Calculate Button
-
 struct HomeView: View {
     
-    @State var currentSugarLevel: Double = 5.5
-    @State var targetSugarLevel: Double = 5.5
+    let buttonImages = ["highlighter", "barcode", "camera"]
     
-    @State private var didLongPress = false
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
+    @State private var currentSugarLevel: Double = 5.5
+    @State private var targetSugarLevel: Double = 5.5
+    @State private var showButtons = false
     
     var body: some View {
         GeometryReader { geometry in
-            
+            //TODO: add Calculate Button
             ZStack {
-                
-                LinearGradient(Color.darkStart, Color.darkEnd)
-                
-                VStack {
-                    HomeViewSlider(range: 2.0...40.0, label: "Current", sliderValue: $currentSugarLevel)
-                    HomeViewSlider(range: 4.5...10.0, label: "Target", sliderValue: $targetSugarLevel)
-                }
-                
-                VStack {
-                    Spacer()
-                        .frame(height: geometry.size.height * 0.8)
-                    
-                    Button(action: {
-                        let impact = UIImpactFeedbackGenerator(style: .heavy)
-                        impact.impactOccurred()
-                    }) {
-                        Image(systemName: "plus")
-                            .font(.system(size: 35, weight: .regular))
-                            .foregroundColor(Color.blue)
+                colorScheme == .light ? LinearGradient(.whiteStart, .whiteEnd) : LinearGradient(.darkStart, .darkEnd)
+                  
+                    VStack(spacing: 10) {
+                        NeumorphicSlider(color: colorScheme, range: 2.0...40.0, label: "Current", sliderValue: $currentSugarLevel)
+                        NeumorphicSlider(color: colorScheme, range: 4.5...10.0, label: "Target", sliderValue: $targetSugarLevel)
                     }
-                    .buttonStyle(HomeViewDarkButtonStyle())
-                }
+                    
+                Spacer()
+                    VStack {
+                        Spacer()
+                            .frame(height: geometry.size.height * 0.7)
+                        
+                        VStack {
+                            HStack {
+                                ForEach(0 ..< 3) { num in
+                                    Button(action: {
+                                        //TODO: add action
+                                    }) {
+                                        Image(systemName: buttonImages[num])
+                                            .font(.system(size: 25, weight: .regular))
+                                            .foregroundColor(.blue)
+                                    }
+                                    .buttonStyle(NeumorphicButtonStyle(paddingSize: 15, color: colorScheme == .light ? .light : .dark))
+                                    .padding()
+                                    .show(isVisible: $showButtons)
+                                }
+                            }
+                        }
+                        
+                        Button(action: {}) {
+                            Image(systemName: "plus")
+                                .font(.system(size: 35, weight: .regular))
+                                .foregroundColor(.blue)
+                        }
+                        .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
+                        .simultaneousGesture(LongPressGesture(minimumDuration: 0.3).onEnded { _ in
+                            let impact = UIImpactFeedbackGenerator(style: .heavy)
+                            impact.impactOccurred()
+                            
+                            withAnimation {
+                                showButtons = true
+                            }
+                            
+                            print("long press Action!")
+                        })
+                        .simultaneousGesture(TapGesture().onEnded {
+                            print("tap Action!")
+                        })
+                        
+                    }
             }
         }
         .edgesIgnoringSafeArea(.all)
+        .onTapGesture {
+            showButtons = false
+        }
     }
 }
 
@@ -54,5 +85,3 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
-
-
