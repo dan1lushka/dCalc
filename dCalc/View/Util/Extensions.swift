9a66410c3a61bf2cab652ea3a499171631d2e79e
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-20 00:49:43 +0000

    add View extensions

---
+++ dCalc/View/Util/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExt.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 18/03/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    static let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+}
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
