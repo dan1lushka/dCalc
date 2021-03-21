955c312a379e7d842d682e3bf09ebf79bfe2cf7b
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-21 15:55:23 +0000

    change refactor HomeView

--- dCalc/View/HomeViews/HomeView.swift
+++ dCalc/View/HomeViews/HomeView.swift
@@ -22,55 +22,58 @@ struct HomeView: View {
             //TODO: add Calculate Button
             ZStack {
                 colorScheme == .light ? LinearGradient(.whiteStart, .whiteEnd) : LinearGradient(.darkStart, .darkEnd)
-                  
-                    VStack(spacing: 10) {
-                        NeumorphicSlider(color: colorScheme, range: 2.0...40.0, label: "Current", sliderValue: $currentSugarLevel)
-                        NeumorphicSlider(color: colorScheme, range: 4.5...10.0, label: "Target", sliderValue: $targetSugarLevel)
+                
+                VStack(spacing: 10) {
+                    NeumorphicSlider(color: colorScheme, range: 2.0...40.0, label: "Current", sliderValue: $currentSugarLevel)
+                    NeumorphicSlider(color: colorScheme, range: 4.5...10.0, label: "Target", sliderValue: $targetSugarLevel)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                        .frame(height: geometry.size.height * 0.7)
+                    
+                    VStack {
+                        HStack {
+                            ForEach(0 ..< 3) { num in
+                                Button(action: {
+                                    //TODO: add action
+                                }) {
+                                    Image(systemName: buttonImages[num])
+                                        .font(.system(size: 25, weight: .regular))
+                                        .foregroundColor(.blue)
+                                }
+                                .buttonStyle(NeumorphicButtonStyle(paddingSize: 15, color: (colorScheme == .light ? .light : .dark)))
+                                .padding()
+                                .show(isVisible: $showButtons)
+                            }
+                        }
                     }
                     
-                Spacer()
-                    VStack {
-                        Spacer()
-                            .frame(height: geometry.size.height * 0.7)
+                    Button(action: {
                         
-                        VStack {
-                            HStack {
-                                ForEach(0 ..< 3) { num in
-                                    Button(action: {
-                                        //TODO: add action
-                                    }) {
-                                        Image(systemName: buttonImages[num])
-                                            .font(.system(size: 25, weight: .regular))
-                                            .foregroundColor(.blue)
-                                    }
-                                    .buttonStyle(NeumorphicButtonStyle(paddingSize: 15, color: colorScheme == .light ? .light : .dark))
-                                    .padding()
-                                    .show(isVisible: $showButtons)
-                                }
-                            }
-                        }
-                        
-                        Button(action: {}) {
-                            Image(systemName: "plus")
-                                .font(.system(size: 35, weight: .regular))
-                                .foregroundColor(.blue)
-                        }
-                        .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
-                        .simultaneousGesture(LongPressGesture(minimumDuration: 0.3).onEnded { _ in
-                            let impact = UIImpactFeedbackGenerator(style: .heavy)
-                            impact.impactOccurred()
-                            
-                            withAnimation {
-                                showButtons = true
-                            }
-                            
-                            print("long press Action!")
-                        })
-                        .simultaneousGesture(TapGesture().onEnded {
-                            print("tap Action!")
-                        })
-                        
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.system(size: 35, weight: .regular))
+                            .foregroundColor(.blue)
                     }
+                    .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
+                    .simultaneousGesture(LongPressGesture(minimumDuration: 0.3).onEnded { _ in
+                        let impact = UIImpactFeedbackGenerator(style: .heavy)
+                        impact.impactOccurred()
+                        
+                        withAnimation {
+                            showButtons = true
+                        }
+                        
+                        print("long press Action!")
+                    })
+                    .simultaneousGesture(TapGesture().onEnded {
+                        print("tap Action!")
+                    })
+                    
+                }
             }
         }
         .edgesIgnoringSafeArea(.all)
