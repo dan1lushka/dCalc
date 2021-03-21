82f1bc531060f9c8670fcfc861a26191a9651f24
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-21 16:16:57 +0000

    change refactor HomeView

--- dCalc/View/HomeViews/HomeView.swift
+++ dCalc/View/HomeViews/HomeView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct HomeView: View {
     
-    let buttonImages = ["highlighter", "barcode", "camera"]
-    
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     
     @State private var currentSugarLevel: Double = 5.5
@@ -23,56 +21,15 @@ struct HomeView: View {
             ZStack {
                 colorScheme == .light ? LinearGradient(.whiteStart, .whiteEnd) : LinearGradient(.darkStart, .darkEnd)
                 
-                VStack(spacing: 10) {
-                    NeumorphicSlider(color: colorScheme, range: 2.0...40.0, label: "Current", sliderValue: $currentSugarLevel)
-                    NeumorphicSlider(color: colorScheme, range: 4.5...10.0, label: "Target", sliderValue: $targetSugarLevel)
-                }
-                
-                Spacer()
-                
                 VStack {
                     Spacer()
-                        .frame(height: geometry.size.height * 0.7)
+                        .frame(height: geometry.size.height * 0.45)
                     
-                    VStack {
-                        HStack {
-                            ForEach(0 ..< 3) { num in
-                                Button(action: {
-                                    //TODO: add action
-                                }) {
-                                    Image(systemName: buttonImages[num])
-                                        .font(.system(size: 25, weight: .regular))
-                                        .foregroundColor(.blue)
-                                }
-                                .buttonStyle(NeumorphicButtonStyle(paddingSize: 15, color: (colorScheme == .light ? .light : .dark)))
-                                .padding()
-                                .show(isVisible: $showButtons)
-                            }
-                        }
-                    }
+                    HomeViewSlider(currentSugarLevel: $currentSugarLevel, targetSugarLevel: $targetSugarLevel, colorScheme: colorScheme)
                     
-                    Button(action: {
-                        
-                    }) {
-                        Image(systemName: "plus")
-                            .font(.system(size: 35, weight: .regular))
-                            .foregroundColor(.blue)
-                    }
-                    .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
-                    .simultaneousGesture(LongPressGesture(minimumDuration: 0.3).onEnded { _ in
-                        let impact = UIImpactFeedbackGenerator(style: .heavy)
-                        impact.impactOccurred()
-                        
-                        withAnimation {
-                            showButtons = true
-                        }
-                        
-                        print("long press Action!")
-                    })
-                    .simultaneousGesture(TapGesture().onEnded {
-                        print("tap Action!")
-                    })
+                    HomeViewExtraButtons(showButtons: $showButtons, colorScheme: colorScheme)
                     
+                    HomeViewAddButton(showButtons: $showButtons, colorScheme: colorScheme)
                 }
             }
         }
