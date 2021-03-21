fa6f8e0be1bf41ef0a241bc4bc1710c50e540161
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-21 16:17:14 +0000

    add HomeViewAddButton

---
+++ dCalc/View/HomeViews/HomeViewAddButton.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewAddButton.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 21/03/2021.
+//
+
+import SwiftUI
+
+struct HomeViewAddButton: View {
+    
+    @Binding var showButtons: Bool
+    
+    var colorScheme: ColorScheme
+    
+    var body: some View {
+        Button(action: {
+            
+        }) {
+            Image(systemName: "plus")
+                .font(.system(size: 35, weight: .regular))
+                .foregroundColor(.blue)
+        }
+        .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
+        .simultaneousGesture(LongPressGesture(minimumDuration: 0.3).onEnded { _ in
+            let impact = UIImpactFeedbackGenerator(style: .heavy)
+            impact.impactOccurred()
+            
+            withAnimation {
+                showButtons = true
+            }
+            
+            print("long press Action!")
+        })
+        .simultaneousGesture(TapGesture().onEnded {
+            print("tap Action!")
+        })
+    }
+}
+
+struct HomeViewAddButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewAddButton(showButtons: .constant(true), colorScheme: .light)
+    }
+}
