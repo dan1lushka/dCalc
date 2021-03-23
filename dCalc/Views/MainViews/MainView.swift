324c65bcd955f59e1f6cd58b11b558c7c1155100
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-23 10:18:35 +0000

    change rename View folder to Views

---
+++ dCalc/Views/MainViews/MainView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 18/03/2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @Binding var selectedIndex: Int
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        HomeView()
+                            .navigationTitle("Home")
+                    }
+                    
+                default:
+                    NavigationView {
+                        ListView()
+                            .navigationTitle("List")
+                    }
+                }
+                
+                VStack {
+                    Spacer()
+                    CustomTabBar(selectedIndex: $selectedIndex, spacing: geometry.size.width * 0.6)
+                }
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MainView(selectedIndex: .constant(0))
+            MainView(selectedIndex: .constant(1))
+        }
+        
+    }
+}
+
+
+
