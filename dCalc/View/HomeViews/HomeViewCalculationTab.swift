37b8c4df12d74b2145f60ed24fe9bc6caefcf04b
Daniel Rybak <daniel.rybak91@gmail.com>
2021-03-23 04:57:18 +0000

    add HomeViewCalculationTab

---
+++ dCalc/View/HomeViews/HomeViewCalculationTab.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewCalculationTab.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 23/03/2021.
+//
+
+import SwiftUI
+
+struct HomeViewCalculationTab: View {
+    
+    @Binding var dosage: Int
+    
+    var colorScheme: ColorScheme
+    
+    var body: some View {
+        ZStack {
+            NeumorphicBackground(color: colorScheme, isHighlighted: false, shape: Rectangle())
+                .cornerRadius(20)
+                .padding(.horizontal, 10)
+                .frame(height: 100)
+            HStack {
+                HomeViewCalculateButton(colorScheme: colorScheme)
+                
+                Spacer()
+                
+                HomeViewDosageLabel(dosage: $dosage, colorScheme: colorScheme)
+            }
+        }
+    }
+}
+
+struct HomeViewCalculationTab_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewCalculationTab(dosage: .constant(2), colorScheme: .light)
+    }
+}
