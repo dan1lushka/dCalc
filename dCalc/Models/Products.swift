7516ce5d0dfb0a015a8a3d969ee85f615bcd1657
Daniel Rybak <daniel.rybak91@gmail.com>
2021-04-02 17:16:47 +0100

    add Products Model

---
+++ dCalc/Models/Products.swift
@@ -0,0 +1,19 @@
+//
+//  Products.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 02/04/2021.
+//
+
+import Foundation
+
+class Products {
+    struct Item {
+        var productName: String?
+        var grammsConsumed: String?
+        var grammsPer100g: String?
+    }
+
+    var items: [Item] = []
+}
+
