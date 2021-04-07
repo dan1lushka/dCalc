e912e6817a50c766a3e86eb51e0b3fe7380f62c4
Daniel Rybak <daniel.rybak91@gmail.com>
2021-04-07 21:08:49 +0100

    add func getDosage() to CalculationManager

--- dCalc/ViewModels/CalculationManager.swift
+++ dCalc/ViewModels/CalculationManager.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 class CalculationManager: ObservableObject {
-    @Published var currentSugarLevel: Double = 5.5
+    
+    @Published var currentSugarLevel: Double = 7.0
     @Published var targetSugarLevel: Double = 5.5
-    @Published var carbsPerUnitOfInsuline: Double = 10.0
+    @Published var carbsPerUnitOfInsulin: Double = 10.0
+    @Published var mmolPerUnitOfInsulin: Double = 3.0
+    
     @Published var dosage = 0
 
     @Published var productName = ""
     @Published var grammsConsumed = ""
-    @Published var grammsPer100g = ""
+    @Published var grammsPer100g: String  = ""
     
-    var products = Products()
+    @Published var consumedFoodItems = ConsumedFood()
+    
+    func getDosage() -> Int {
+        if currentSugarLevel <= targetSugarLevel {
+            return 0
+        }
+        
+        let totalCarbs = consumedFoodItems.items.map({ $0.grammsConsumed / 100 * $0.grammsPer100g }).reduce(0.0, +)
+        
+        let unitsForTotalCarbs = totalCarbs / carbsPerUnitOfInsulin
+        
+        let currentToTargetSugarLevelDifference = currentSugarLevel - targetSugarLevel
+        
+        let unitsOfInsulinForSugarLevelDifference = currentToTargetSugarLevelDifference / mmolPerUnitOfInsulin
+        
+        let result = unitsForTotalCarbs + unitsOfInsulinForSugarLevelDifference
+
+        return Int(result)
+    }
 }
