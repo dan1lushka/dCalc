9128e26ebded37f49a9bd06ac06481cfb031ad74
Daniel Rybak <daniel.rybak91@gmail.com>
2021-06-03 17:32:14 +0100

    add K file for constants

---
+++ dCalc/Util/K.swift
@@ -0,0 +1,16 @@
+//
+//  K.swift
+//  dCalc
+//
+//  Created by Daniel Rybak on 01/06/2021.
+//
+
+import Foundation
+
+struct NetworkingConsts {
+  static let apiID = "4ccff81d"
+  static let apiKey = "e79e643e49de8cfd4b1425baa583798a"
+}
+
+//http://api.edamam.com/auto-complete?q=Apple&limit=10&app_id=4ccff81d&app_key=e79e643e49de8cfd4b1425baa583798a
+//https://api.edamam.com/api/food-database/v2/parser?ingr=Chicken&app_id=4ccff81d&app_key=e79e643e49de8cfd4b1425baa583798a
