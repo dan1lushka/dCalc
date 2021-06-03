592e1c57dbe6b62c3af83e0911070f089a8ec9a1
Daniel Rybak <daniel.rybak91@gmail.com>
2021-06-03 17:33:41 +0100

    change fix styling in HomeViewAddButton

--- dCalc/Views/HomeViews/Buttons/HomeViewAddButton.swift
+++ dCalc/Views/HomeViews/Buttons/HomeViewAddButton.swift
@@ -14,14 +14,16 @@ struct HomeViewAddButton: View {
     @ObservedObject var viewTransitionManager: ViewTransitionManager
 
     var body: some View {
-        Button(action: {}) {
+      Button {
+        
+      } label: {
             Image(systemName: "plus")
                 .font(.system(size: 35, weight: .regular))
                 .foregroundColor(.cornBlue)
         }
         .contentShape(Circle())
         .buttonStyle(NeumorphicButtonStyle(paddingSize: 25, color: colorScheme == .light ? .light : .dark))
-        //FIXME: simultaneousGesture activates TapGesture when LongPressGesture is executed
+        // fixme: simultaneousGesture activates TapGesture when LongPressGesture is executed
         .simultaneousGesture(LongPressGesture(minimumDuration: 0.5)
                                 .onEnded { _ in
                                     let impact = UIImpactFeedbackGenerator(style: .heavy)
