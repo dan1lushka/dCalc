2fa41048a7e9755e3cdd9c850996d8c5b857b085
Daniel Rybak <daniel.rybak91@gmail.com>
2021-06-03 17:36:46 +0100

    change refactor NetworkingManager to work with generic types

--- dCalc/ViewModels/NetworkingManager.swift
+++ dCalc/ViewModels/NetworkingManager.swift
@@ -8,94 +8,133 @@
 import SwiftUI
 
 class NetworkingManager: ObservableObject {
+  
+  enum RequestType {
+    case autoCompleteSearch
+    case parserSearch
+  }
+  
+  @Published var response: Codable?
+  @Published var errorMessage = ""
+  
+  func loadData<T: Codable>(ingredient: String, objectType: T.Type, callback: @escaping () -> Void) {
     
-    @Published var response: FoodInfoResponse?
-    @Published var errorMessage = ""
+    let request = getURLRequest(ingredient: ingredient, objectType: objectType)
     
-    func loadData(ingredient: String, callback: @escaping () -> ()) {
+    guard let request = request else {
+      self.errorMessage = "URL Request error"
+      return
+    }
+    
+    print(request.httpBody ?? "no request body found")
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      if let error = error {
+        self.errorMessage = "\(error.localizedDescription)"
+        print("\(error.localizedDescription)")
+        callback()
+      } else {
         
-        guard let request = getURLRequest(ingredient: ingredient) else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            
-            if let data = data {
-                
-                DispatchQueue.main.async {
-                    self.performResponseDecoding(data: data)
-                    callback()
-                }
-                
-                return
-            } else {
-                self.errorMessage = "Cannot get data"
-                print("Cannot get data")
+        if let response = response as? HTTPURLResponse {
+          if response.statusCode != 200 {
+            DispatchQueue.main.async {
+              self.errorMessage = "Server error \(response.statusCode)"
+              print("Server responds with \(response.statusCode) Not Found")
+              callback()
             }
-            
-        }.resume()
+          }
+        }
         
+        if let data = data {
+          DispatchQueue.main.async {
+            self.performResponseDecoding(data: data, objectType: objectType)
+            callback()
+          }
+        }
+        
+      }
+    }.resume()
+  }
+  
+  private func getURLRequest<T: Codable>(ingredient: String, objectType: T.Type) -> URLRequest? {
+    print(ingredient)
+    
+    let appIdQueryItem = URLQueryItem(name: "app_id", value: NetworkingConsts.apiID)
+    let appKeyQueryItem = URLQueryItem(name: "app_key", value: NetworkingConsts.apiKey)
+    
+    var urlPath: String
+    var requestQueryItem: URLQueryItem
+    var queryItems: [URLQueryItem]
+    
+    // todo: move to separate service
+    switch objectType {
+    case is FoodInfoResponse.Type:
+      urlPath = "api/food-database/v2/parser?"
+      requestQueryItem = URLQueryItem(name: "ingr", value: ingredient)
+      queryItems = [
+        requestQueryItem,
+        appIdQueryItem,
+        appKeyQueryItem
+      ]
+    case is AutoCompleteResponse.Type:
+      urlPath = "auto-complete?"
+      requestQueryItem = URLQueryItem(name: "q", value: ingredient)
+      queryItems = [
+        requestQueryItem
+      ]
+    default:
+      self.errorMessage = "Could not infer type for decoding"
+      return nil
+    }
+
+    var components = URLComponents(string: "https://api.edamam.com/\(urlPath))")!
+
+    components.queryItems = queryItems
+
+    guard let url = components.url else {
+      self.errorMessage = "Invalid URL"
+      return nil
     }
     
-    private func getURLRequest(ingredient: String) -> URLRequest? {
-        print(ingredient)
-        
-        let apiID = "4ccff81d"
-        let apiKey = "e79e643e49de8cfd4b1425baa583798a"
-        
-        var components = URLComponents(string: "https://api.edamam.com/api/food-database/v2/parser?")!
-        
-        components.queryItems = [
-            URLQueryItem(name: "ingr", value: ingredient),
-            URLQueryItem(name: "app_id", value: apiID),
-            URLQueryItem(name: "app_key", value: apiKey)
-        ]
-        
-        guard let url = components.url else {
-            self.errorMessage = "Invalid url"
-            return nil
-        }
-        
-        print(url)
-        
-        return URLRequest(url: url)
+    print(url)
+    
+    return URLRequest(url: url)
+  }
+  
+  private func performResponseDecoding<T: Codable>(data: Data, objectType: T.Type) {
+    
+    var isError = false
+    
+    do {
+      let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+      self.response = decodedResponse
+      
+    } catch let DecodingError.dataCorrupted(context) {
+      isError = true
+      print(context.codingPath)
+      
+    } catch let DecodingError.keyNotFound(key, context) {
+      isError = true
+      print("Key '\(key)' not found:", context.debugDescription)
+      print("codingPath:", context.codingPath)
+      
+    } catch let DecodingError.valueNotFound(value, context) {
+      isError = true
+      print("Value '\(value)' not found:", context.debugDescription)
+      print("codingPath:", context.codingPath)
+      
+    } catch let DecodingError.typeMismatch(type, context) {
+      isError = true
+      print("Type '\(type)' mismatch:", context.debugDescription)
+      print("codingPath:", context.codingPath)
+      
+    } catch {
+      isError = true
+      print("error: ", error)
     }
     
-    func performResponseDecoding(data: Data) {
-        
-        var isError = false
-        
-        do {
-            let decodedResponse = try JSONDecoder().decode(FoodInfoResponse.self, from: data)
-            self.response = decodedResponse
-            print(self.response?.parsed.first?.food.nutrients.chocdf ?? "carbs not found")
-            
-        } catch let DecodingError.dataCorrupted(context) {
-            isError = true
-            print(context.debugDescription)
-            
-        } catch let DecodingError.keyNotFound(key, context) {
-            isError = true
-            print("Key '\(key)' not found:", context.debugDescription)
-            print("codingPath:", context.codingPath)
-            
-        } catch let DecodingError.valueNotFound(value, context) {
-            isError = true
-            print("Value '\(value)' not found:", context.debugDescription)
-            print("codingPath:", context.codingPath)
-            
-        } catch let DecodingError.typeMismatch(type, context)  {
-            isError = true
-            print("Type '\(type)' mismatch:", context.debugDescription)
-            print("codingPath:", context.codingPath)
-            
-        } catch {
-            isError = true
-            print("error: ", error)
-        }
-        
-        if isError {
-            self.errorMessage = "An unexpected error occured"
-        }
+    if isError {
+      self.errorMessage = "An unexpected error occured"
     }
+  }
 }
