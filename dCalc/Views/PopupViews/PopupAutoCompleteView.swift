5c51fc472eff700a62b1fcc16c774b4d5147c8ce
Daniel Rybak <daniel.rybak91@gmail.com>
2021-06-09 09:15:36 +0100

    change put placeholder code for PopupAutoCompleteView to show async image

--- dCalc/Views/PopupViews/PopupAutoCompleteView.swift
+++ dCalc/Views/PopupViews/PopupAutoCompleteView.swift
@@ -11,7 +11,7 @@ struct PopupAutoCompleteView: View {
   
   var height: CGFloat
   var width: CGFloat
-  var colorScheme: ColorScheme
+  @Environment(\.colorScheme) var colorScheme: ColorScheme
   
   @ObservedObject var calculationManager: CalculationManager
   @ObservedObject var popupViewManager: PopupViewManager
@@ -22,23 +22,35 @@ struct PopupAutoCompleteView: View {
   var body: some View {
     VStack {
       ScrollView {
-        if let stringArray = popupViewManager.autoCompleteList as? AutoCompleteResponse {
-          ForEach(stringArray, id: \.self) { item in
-            Text("\(item)")
-              .frame(width: width * 0.95, height: 20, alignment: .leading)
-              .padding(.horizontal, 10)
-              .padding(.vertical, 5)
-              .onTapGesture {
-                popupViewManager.isEditingProductName = false
-                calculationManager.productName = String(item)
-                hideKeyboard()
-                print("tapped \(item) item")
+        if let nameArray = popupViewManager.autoCompleteList as? AutoCompleteResponse {
+          ForEach(nameArray, id: \.self) { item in
+            HStack {
+              if #available(iOS 15.0, *) {
+                AsyncImage(url: URL(string: popupViewManager.imageURLList.first ?? "")) { image in
+                  image.resizable(resizingMode: .tile)
+                } placeholder: {
+                  Image(systemName: "arrow.down.doc.fill")
+                }
+                .frame(width: 25, height: 20, alignment: .center)
+                .padding(.leading, 10)
               }
+              Text("\(item)")
+            }
+            .frame(width: width * 0.95, height: 20, alignment: .leading)
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .onTapGesture {
+              popupViewManager.isEditingProductName = false
+              calculationManager.productName = String(item)
+              hideKeyboard()
+              print("tapped \(item) item")
+            }
+            Divider()
           }
         }
       }
       .frame(width: width * 0.95, height: height / 2.5, alignment: .bottom)
-      .background(NeumorphicBackground(color: colorScheme, isHighlighted: false, shape: Rectangle()))
+      .background(NeumorphicBackground(isHighlighted: false, shape: Rectangle()))
       .padding(.horizontal, 10)
       .position(x: width / 2, y: height / 3)
       .show(isVisible: $isEditingProductName)
@@ -50,7 +62,6 @@ struct PopupAutcompleteView_Previews: PreviewProvider {
   static var previews: some View {
     PopupAutoCompleteView(height: 300,
                           width: 300,
-                          colorScheme: .light,
                           calculationManager: CalculationManager(),
                           popupViewManager: PopupViewManager(),
                           networkingManager: NetworkingManager(),
